e7405d1d9aed4f9c05f7190decce742c3a60fa47
arnobl <1052622+arnobl@users.noreply.github.com>
2012-10-23 12:04:09 +0200

    Added: a link to convert a button pressed interaction into an action that shows a component.

--- src/org/malai/instrument/library/Interaction2ShowComponentLink.java
+++ src/org/malai/instrument/library/Interaction2ShowComponentLink.java
@@ -2,12 +2,13 @@ package org.malai.instrument.library;
 
 import java.awt.Component;
 
+import org.malai.action.library.ShowWidget;
 import org.malai.instrument.Instrument;
-import org.malai.interaction.library.MenuItemPressed;
-import org.malai.widget.MMenuItem;
+import org.malai.instrument.Link;
+import org.malai.interaction.Interaction;
 
 /**
- * This link links a menu item interaction to an action that shows a JComponent.<br>
+ * This link maps an interaction to an action that shows a JComponent.<br>
  * <br>
  * This file is part of Malai.<br>
  * Copyright (c) 2009-2012 Arnaud BLOUIN<br>
@@ -20,32 +21,34 @@ import org.malai.widget.MMenuItem;
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.<br>
  * <br>
- * 11/20/2010<br>
+ * 2012-10-23<br>
  * @author Arnaud BLOUIN
  * @since 0.2
  * @param <N> The type of the instrument that will contain this link.
+ * @param <I> The type of the interaction.
  */
-public class MenuItem2ShowComponentLink<N extends Instrument> extends Interaction2ShowComponentLink<MenuItemPressed, N> {
-	/** The menu item used to shows the component. */
-	protected MMenuItem menuItem;
+public abstract class Interaction2ShowComponentLink<I extends Interaction, N extends Instrument> extends Link<ShowWidget, I, N> {
+	/** The component to show. */
+	protected Component component;
 
 	/**
 	 * Creates the link.
 	 * @param ins The instrument that contains the link.
 	 * @param component The component to show/hide.
-	 * @param menuItem The menu item used to show/hide to component.
+	 * @param exec Specifies if the action must be execute or update on each evolution of the interaction.
+	 * @param interactionClass The class corresponding to the interaction of the link.
 	 * @throws IllegalAccessException If no free-parameter constructor is available.
 	 * @throws InstantiationException If an error occurs during instantiation of the interaction/action.
-	 * @since 0.2
 	 */
-	public MenuItem2ShowComponentLink(final N ins, final Component component, final MMenuItem menuItem) throws InstantiationException, IllegalAccessException {
-		super(ins, false, MenuItemPressed.class, component);
-		this.menuItem	= menuItem;
+	public Interaction2ShowComponentLink(final N ins, final boolean exec, final Class<I> interactionClass, final Component component)
+			throws InstantiationException, IllegalAccessException {
+		super(ins, exec, ShowWidget.class, interactionClass);
+		this.component	= component;
 	}
 
-
 	@Override
-	public boolean isConditionRespected() {
-		return interaction.getMenuItem()==menuItem;
+	public void initAction() {
+		action.setComponent(component);
+		action.setVisible(true);
 	}
 }
