f7419b6a991b896bd54d4a8d7b2d5d145b5044c9
arnobl <blouin.arno@gmail.com>
2013-03-02 11:14:30 +0100

    Moving project

---
+++ malai-example/org.malai.example.android.drawingEditor/src/org/malai/example/drawingEditor/action/AddShape.java
@@ -0,0 +1,29 @@
+package org.malai.example.drawingEditor.action;
+
+import org.malai.action.Action;
+
+public class AddShape extends Action {
+
+	public AddShape() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean canDo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void doActionBody() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isRegisterable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
