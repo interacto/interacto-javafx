123af447b318b1a165a8c303cf5891f9c1d9e2f8
arnobl <1052622+arnobl@users.noreply.github.com>
2013-03-02 11:14:30 +0100

    Moving project

--- malai-example/org.malai.example.android.drawingEditor/src/org/malai/example/drawingEditor/view/ViewShape.java
+++ malai-example/org.malai.example.android.drawingEditor/src/org/malai/example/drawingEditor/view/ViewShape.java
@@ -3,10 +3,13 @@ package org.malai.example.drawingEditor.view;
 import org.malai.picking.Pickable;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public abstract class ViewShape extends View implements Pickable {
+	protected Paint paint = new Paint();
+	
 	public ViewShape(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
