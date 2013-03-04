72fdb9fb0b9b65ffe529cb3356f8e572c54c8689
Maxime Lorant <maxime.lorant@gmail.com>
2013-03-04 22:28:10 +0100

    Begin of the wiimote implementation, inspirate on the Android impl.

--- malai-core/org.malai.wiimote/src/main/org/malai/wiimote/interaction/WiimoteEventHandler.java
+++ malai-core/org.malai.wiimote/src/main/org/malai/wiimote/interaction/WiimoteEventHandler.java
@@ -1,5 +1,34 @@
 package org.malai.wiimote.interaction;
 
-public interface WiimoteEventHandler {
+import wiiusej.wiiusejevents.physicalevents.ButtonsEvent;
 
+/**
+ * This interface can be used for object that want to gather Wiimote events (button pressed, moves, etc.) produced by HIDs.<br>
+ * <br>
+ * This file is part of Malai.<br>
+ * Copyright (c) 2009-2013 Tom Demulier--Chevret, Juliette Gourlaouen, Maxime Lorant, Liantsoa Rasata-Manantena <br>
+ * <br>
+ * Malai is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * <br>
+ * Malai is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.<br>
+ * <br>
+ * 2013-02-26<br>
+ * @author Tom Demulier--Chevret
+ * @author Juliette Gourlaouen
+ * @author Maxime Lorant
+ * @author Liantsoa Rasata-Manantena
+ * @since 0.2
+ */
+public interface WiimoteEventHandler {
+	
+	/**
+	 * Defines actions to do when a button is activated.
+	 * @param button The pressed button.
+	 * @since 0.2
+	 */
+	void onButtonPressed(final ButtonsEvent button);
 }
--- malai-core/org.malai.wiimote/src/main/org/malai/wiimote/interaction/WiimoteInteraction.java
+++ malai-core/org.malai.wiimote/src/main/org/malai/wiimote/interaction/WiimoteInteraction.java
@@ -1,5 +1,42 @@
 package org.malai.wiimote.interaction;
 
-public class WiimoteInteraction {
+import org.malai.interaction.InitState;
+import org.malai.interaction.Interaction;
+import org.malai.stateMachine.ITransition;
+
+import wiiusej.wiiusejevents.physicalevents.ButtonsEvent;
+
+public class WiimoteInteraction extends Interaction implements WiimoteEventHandler {
+
+	public WiimoteInteraction() {
+		super();
+	}
+	
+	public WiimoteInteraction(final InitState initState) {
+		super(initState);
+	}
+	
+	public void onButtonPressed(ButtonsEvent button) {
+		if(!activated) return ;
+
+		boolean again = true;
+		ITransition t;
+
+		for(int i=0, j=currentState.getTransitions().size(); i<j && again; i++) {
+			t = currentState.getTransition(i);
+
+			if(t instanceof ButtonPressedTransition) {
+				((ButtonPressedTransition)t).setButton(button);
+				again = !checkTransition(t);
+			}
+		}
+		
+	}
+
+	@Override
+	protected void initStateMachine() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
