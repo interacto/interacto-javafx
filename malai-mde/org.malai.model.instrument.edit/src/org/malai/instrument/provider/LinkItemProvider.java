8e894887be26d011844c9b330a80667153d1b837
arnobl <1052622+arnobl@users.noreply.github.com>
2013-03-27 16:53:45 +0100

    fixed: problem when defining the class of a link in the instrument editor.

--- malai-mde/org.malai.model.instrument.edit/src/org/malai/instrument/provider/LinkItemProvider.java
+++ malai-mde/org.malai.model.instrument.edit/src/org/malai/instrument/provider/LinkItemProvider.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
@@ -67,7 +69,6 @@ public class LinkItemProvider
 			addUpdateActionPropertyDescriptor(object);
 			addExecuteOnUpdatePropertyDescriptor(object);
 			addInitialiseActionPropertyDescriptor(object);
-			addClazzPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
@@ -249,25 +250,33 @@ public class LinkItemProvider
 	}
 
 	/**
-	 * This adds a property descriptor for the Clazz feature.
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	protected void addClazzPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_Link_clazz_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_Link_clazz_feature", "_UI_Link_type"),
-				 InstrumentPackage.Literals.LINK__CLAZZ,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(InstrumentPackage.Literals.LINK__CLAZZ);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
 	}
 
 	/**
@@ -325,6 +334,9 @@ public class LinkItemProvider
 			case InstrumentPackage.LINK__INITIALISE_ACTION:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
+			case InstrumentPackage.LINK__CLAZZ:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
 		}
 		super.notifyChanged(notification);
 	}
@@ -339,6 +351,11 @@ public class LinkItemProvider
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(InstrumentPackage.Literals.LINK__CLAZZ,
+				 EcoreFactory.eINSTANCE.createEClass()));
 	}
 
 	/**
