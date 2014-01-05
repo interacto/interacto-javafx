796e375c2514f207539eaa75fbeea0b8b195b462
arnobl <1052622+arnobl@users.noreply.github.com>
2014-01-05 11:58:16 +0100

    Move to 2014

--- malai-core/org.malai.swing/src/main/org/malai/swing/instrument/WidgetInstrument.java
+++ malai-core/org.malai.swing/src/main/org/malai/swing/instrument/WidgetInstrument.java
@@ -7,7 +7,7 @@ import org.malai.swing.ui.UIComposer;
  * Defines an abstract model of an instrument that has widgets.<br>
  * <br>
  * This file is part of libMalai.<br>
- * Copyright (c) 2009-2013 Arnaud BLOUIN<br>
+ * Copyright (c) 2005-2014 Arnaud BLOUIN<br>
  * <br>
  * libMalan is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
