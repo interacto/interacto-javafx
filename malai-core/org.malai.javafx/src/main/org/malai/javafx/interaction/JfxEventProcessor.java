c2a49e4d2eaa5a61b777395eac0f7d446a8c8b5e
arnobl <1052622+arnobl@users.noreply.github.com>
2014-09-19 17:34:21 +0200

    Completing the initial implementation

---
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/interaction/JfxEventProcessor.java
@@ -0,0 +1,35 @@
+package org.malai.javafx.interaction;
+
+import javafx.scene.control.ButtonBase;
+import javafx.scene.control.ColorPicker;
+
+/**
+ * Defines all the events an JavaFX interaction must manage.<br>
+ * <br>
+ * This file is part of libMalai.<br>
+ * Copyright (c) 2005-2014 Arnaud BLOUIN<br>
+ * <br>
+ * libMalan is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.<br>
+ * <br>
+ * libMalan is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.<br>
+ * @author Arnaud BLOUIN
+ * @date 2014-09-19
+ * @version 2.0
+ */
+public interface JfxEventProcessor {
+	/**
+	 * Defines actions to do when a button is triggered.
+	 * @param button The pressed button.
+	 */
+	void onJfxButtonPressed(final ButtonBase button);
+	
+	/**
+	 * Defines actions to do when a colour is selected.
+	 * @param picker the colour picker.
+	 */
+	void onJfxColorPicked(final ColorPicker picker);
+}
