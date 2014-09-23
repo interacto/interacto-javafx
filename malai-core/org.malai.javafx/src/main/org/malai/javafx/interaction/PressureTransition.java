2f132286ea0e9ab86a347911f6a22cb57b19eda9
arnobl <blouin.arno@gmail.com>
2014-09-23 23:22:50 +0200

    Adding mouse transitions to the JavaFX module

---
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/interaction/PressureTransition.java
@@ -0,0 +1,30 @@
+package org.malai.javafx.interaction;
+
+import org.malai.stateMachine.SourceableState;
+import org.malai.stateMachine.TargetableState;
+
+/**
+ * This transition corresponds to a pressure of a button of a pointing device.<br>
+ * <br>
+ * This file is part of Malai.<br>
+ * Copyright (c) 2005-2014 Arnaud BLOUIN<br>
+ * <br>
+ * Malai is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * <br>
+ * Malai is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.<br>
+ * <br>
+ * 2014-09-23<br>
+ * @author Arnaud BLOUIN
+ */
+public class PressureTransition extends PointingDeviceTransition {
+	/**
+	 * Creates the transition.
+	 */
+	public PressureTransition(final SourceableState inputState, final TargetableState outputState) {
+		super(inputState, outputState);
+	}
+}
---
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/interaction/ReleaseTransition.java
@@ -0,0 +1,34 @@
+package org.malai.javafx.interaction;
+
+import org.malai.stateMachine.SourceableState;
+import org.malai.stateMachine.TargetableState;
+
+/**
+ * This transition corresponds to a release of a button of a pointing device.<br>
+ * <br>
+ * This file is part of Malai.<br>
+ * Copyright (c) 2005-2014 Arnaud BLOUIN<br>
+ * <br>
+ * Malai is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * <br>
+ * Malai is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.<br>
+ * <br>
+ * 2014-09-23<br>
+ * @author Arnaud BLOUIN
+ */
+public class ReleaseTransition extends PointingDeviceTransition {
+	/**
+	 * Defines a transition.
+	 * @param inputState The source state of the transition.
+	 * @param outputState The target state of the transition.
+	 * @throws IllegalArgumentException If one of the given parameters is null or not valid.
+	 * @since 0.1
+	 */
+	public ReleaseTransition(final SourceableState inputState, final TargetableState outputState) {
+		super(inputState, outputState);
+	}
+}
