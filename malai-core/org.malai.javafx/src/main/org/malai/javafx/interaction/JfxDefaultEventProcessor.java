536891763ff6d041b0b8ca33227290745f00e343
arnobl <1052622+arnobl@users.noreply.github.com>
2014-12-21 17:54:38 +0100

    Spinner-based interactions added

--- malai-core/org.malai.javafx/src/main/org/malai/javafx/interaction/JfxDefaultEventProcessor.java
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/interaction/JfxDefaultEventProcessor.java
@@ -1,15 +1,6 @@
 package org.malai.javafx.interaction;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
@@ -185,6 +176,15 @@ public interface JfxDefaultEventProcessor extends JfxEventProcessor, Interaction
 		if(!isActivated()) return ;
 		getCurrentState().getTransitions().stream().filter(tr -> tr instanceof JfxTextChangedTransition).filter(tr -> {
 			((JfxTextChangedTransition)tr).setWidget(cc);
+			return checkTransition(tr);
+		}).findFirst();
+	}
+	
+	@Override
+	default void onJfxSpinnerValueChanged(final Spinner<?> spinner) {
+		if(!isActivated()) return ;
+		getCurrentState().getTransitions().stream().filter(tr -> tr instanceof JfxSpinnerValueChangedTransition).filter(tr -> {
+			((JfxSpinnerValueChangedTransition)tr).setWidget(spinner);
 			return checkTransition(tr);
 		}).findFirst();
 	}
