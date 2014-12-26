838f168d8bedcc3284aa0af32ae7c788a1c2608e
arnobl <1052622+arnobl@users.noreply.github.com>
2014-12-26 16:06:26 +0100

    Code simplification

--- malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/library/ButtonInteractor.java
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/library/ButtonInteractor.java
@@ -40,4 +40,18 @@ public abstract class ButtonInteractor<A extends Action, I extends JfxInstrument
 	public ButtonInteractor(I ins, Class<A> clazzAction, List<Node> widgets) throws InstantiationException, IllegalAccessException {
 		super(ins, false, clazzAction, ButtonPressed.class, widgets);
 	}
+	
+	/**
+	 * Creates an interactor.
+	 * @param ins The instrument that contains the interactor.
+	 * @param clazzAction The type of the action that will be created. Used to instantiate the action by reflexivity.
+	 * The class must be public and must have a constructor with no parameter.
+	 * @param widgets The widgets used by the interactor. Cannot be null.
+	 * @throws IllegalAccessException If no free-parameter constructor is available.
+	 * @throws InstantiationException If an error occurs during instantiation of the interaction/action.
+	 * @throws IllegalArgumentException If the given interaction or instrument is null.
+	 */
+	public ButtonInteractor(I ins, Class<A> clazzAction, Node... widgets) throws InstantiationException, IllegalAccessException {
+		super(ins, false, clazzAction, ButtonPressed.class, widgets);
+	}
 }
