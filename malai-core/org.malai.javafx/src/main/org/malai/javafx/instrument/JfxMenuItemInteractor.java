0b40da032443cd6e56ab2418703aaa90b4e8266e
arnobl <1052622+arnobl@users.noreply.github.com>
2016-05-23 21:51:37 +0200

    feat(jfx): can show / hide nodes and stages

--- malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/JfxMenuItemInteractor.java
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/JfxMenuItemInteractor.java
@@ -4,8 +4,7 @@ import javafx.scene.control.MenuItem;
 import org.malai.action.Action;
 import org.malai.javafx.interaction.library.MenuItemInteraction;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 /**
  * Base of an interactor for JavaFX applications.<br>
@@ -24,24 +23,8 @@ import java.util.List;
  * @version 2.0
  */
 public abstract class JfxMenuItemInteractor<A extends Action, I extends MenuItemInteraction<MenuItem>, N extends JfxInstrument> extends JfxInteractor<A, I, N> {
-	/**
-	 * Creates an interactor. This constructor must initialise the interaction. The interactor is (de-)activated if the given
-	 * instrument is (de-)activated.
-	 * @param ins The instrument that contains the interactor.
-	 * @param exec Specifies if the action must be execute or update on each evolution of the interaction.
-	 * @param clazzAction The type of the action that will be created. Used to instantiate the action by reflexivity.
-	 * The class must be public and must have a constructor with no parameter.
-	 * @param clazzInteraction The type of the interaction that will be created. Used to instantiate the interaction by reflexivity.
-	 * The class must be public and must have a constructor with no parameter.
-	 * @param widgets The widgets used by the interactor. Cannot be null.
-	 * @throws IllegalAccessException If no free-parameter constructor is available.
-	 * @throws InstantiationException If an error occurs during instantiation of the interaction/action.
-	 * @throws IllegalArgumentException If the given interaction or instrument is null.
-	 */
-	public JfxMenuItemInteractor(N ins, boolean exec, Class<A> clazzAction, Class<I> clazzInteraction, List<MenuItem> widgets) throws InstantiationException, IllegalAccessException {
-		super(ins, exec, clazzAction, clazzInteraction);
-		interaction.registerToMenuItems(widgets);
-	}
+	/** The menu item that will be uses to create the action. */
+	protected MenuItem menuItem;
 
 	/**
 	 * Creates an interactor. This constructor must initialise the interaction. The interactor is (de-)activated if the given
@@ -52,18 +35,24 @@ public abstract class JfxMenuItemInteractor<A extends Action, I extends MenuItem
 	 * The class must be public and must have a constructor with no parameter.
 	 * @param clazzInteraction The type of the interaction that will be created. Used to instantiate the interaction by reflexivity.
 	 * The class must be public and must have a constructor with no parameter.
-	 * @param widgets The widgets used by the interactor. Cannot be null.
+	 * @param widget The menu item used by the interactor. Cannot be null.
 	 * @throws IllegalAccessException If no free-parameter constructor is available.
 	 * @throws InstantiationException If an error occurs during instantiation of the interaction/action.
 	 * @throws IllegalArgumentException If the given interaction or instrument is null.
 	 */
-	public JfxMenuItemInteractor(N ins, boolean exec, Class<A> clazzAction, Class<I> clazzInteraction, MenuItem... widgets) throws InstantiationException, IllegalAccessException {
-		this(ins, exec, clazzAction, clazzInteraction, Arrays.asList(widgets));
+	public JfxMenuItemInteractor(N ins, boolean exec, Class<A> clazzAction, Class<I> clazzInteraction, MenuItem widget) throws InstantiationException, IllegalAccessException {
+		super(ins, exec, clazzAction, clazzInteraction);
+
+		if(widget==null)
+			throw new IllegalArgumentException();
+
+		menuItem = widget;
+		interaction.registerToMenuItems(Collections.singletonList(widget));
 	}
 
 
 	@Override
 	public boolean isConditionRespected() {
-		return true;
+		return interaction.getWidget()==menuItem;
 	}
 }
--- malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/library/MenuItem2ShowLazyStage.java
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/library/MenuItem2ShowLazyStage.java
@@ -1,13 +1,13 @@
 package org.malai.javafx.instrument.library;
 
 import javafx.scene.control.MenuItem;
-import org.malai.javafx.action.library.OpenWebPageJFX;
+import javafx.stage.Stage;
+import org.malai.javafx.action.library.ShowStage;
 import org.malai.javafx.instrument.JfxInstrument;
 import org.malai.javafx.instrument.JfxMenuItemInteractor;
 import org.malai.javafx.interaction.library.MenuItemPressed;
 
-import java.net.URI;
-
+import java.util.function.Supplier;
 
 /**
  * An interactor that opens a URL using a menu item.<br>
@@ -27,33 +27,36 @@ import java.net.URI;
  * @author Arnaud BLOUIN
  * @since 2.0
  */
-public class MenuItem2OpenWebPageInteractor extends JfxMenuItemInteractor<OpenWebPageJFX, MenuItemPressed, JfxInstrument> {
-	/** The URI to open. */
-	protected URI uri;
+public class MenuItem2ShowLazyStage extends JfxMenuItemInteractor<ShowStage, MenuItemPressed, JfxInstrument> {
+	protected Supplier<Stage> stageToShowLazy;
+
+	protected boolean show;
 
 	/**
 	 * Creates the interactor.
 	 * @param ins The instrument that will contain the interactor.
 	 * @param menuItem he menu item that will be uses to create the action.
-	 * @param uri The URI to open.
+	 * @param stageLazy The stage to show or hide (the creation of the stage can be postponed at the execution of the action).
 	 * @throws IllegalArgumentException If one of the given parameters is null.
 	 * @throws IllegalAccessException If no free-parameter constructor is available.
 	 * @throws InstantiationException If an error occurs during instantiation of the interaction/action.
 	 * @since 2.0
 	 */
-	public MenuItem2OpenWebPageInteractor(final JfxInstrument ins, final MenuItem menuItem,
-										  final URI uri) throws InstantiationException, IllegalAccessException {
-		super(ins, false, OpenWebPageJFX.class, MenuItemPressed.class, menuItem);
+	public MenuItem2ShowLazyStage(final JfxInstrument ins, final MenuItem menuItem, final Supplier<Stage> stageLazy, final boolean toshow)
+								throws InstantiationException, IllegalAccessException {
+		super(ins, false, ShowStage.class, MenuItemPressed.class, menuItem);
 
-		if(uri==null)
+		if(stageLazy==null)
 			throw new IllegalArgumentException();
 
-		this.uri = uri;
+		stageToShowLazy = stageLazy;
+		show = toshow;
 	}
-
 
 	@Override
 	public void initAction() {
-		action.setUri(uri);
+		System.out.println(stageToShowLazy.get());
+		action.setWidget(stageToShowLazy.get());
+		action.setVisible(show);
 	}
 }
