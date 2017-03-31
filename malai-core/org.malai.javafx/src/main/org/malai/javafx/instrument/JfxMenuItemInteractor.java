d415fba7a0354b9baee61d877a1fa4b6f7f23fb5
arnobl <blouin.arno@gmail.com>
2017-03-31 21:04:19 +0200

    change(interactor): the menu interactor should be able to take several menus when created

--- malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/JfxMenuItemInteractor.java
+++ malai-core/org.malai.javafx/src/main/org/malai/javafx/instrument/JfxMenuItemInteractor.java
@@ -1,30 +1,28 @@
+/*
+ * This file is part of Malai.
+ * Copyright (c) 2005-2017 Arnaud BLOUIN
+ * Malai is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * Malai is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
 package org.malai.javafx.instrument;
 
+import java.util.Arrays;
+import java.util.List;
 import javafx.scene.control.MenuItem;
 import org.malai.action.ActionImpl;
 import org.malai.javafx.interaction.library.MenuItemInteraction;
 
-import java.util.Collections;
-
 /**
- * Base of an interactor for JavaFX applications.<br>
- * <br>
- * This file is part of libMalai.<br>
- * Copyright (c) 2005-2015 Arnaud BLOUIN<br>
- * <br>
- * libMalan is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later version.<br>
- * <br>
- * libMalan is distributed without any warranty; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.<br>
+ * Base of an interactor for JavaFX applications.
  * @author Arnaud BLOUIN
- * @version 2.0
  */
 public abstract class JfxMenuItemInteractor<A extends ActionImpl, I extends MenuItemInteraction<MenuItem>, N extends JfxInstrument> extends JfxInteractor<A, I, N> {
 	/** The menu item that will be uses to create the action. */
-	protected MenuItem menuItem;
+	protected final List<MenuItem> menuItem;
 
 	/**
 	 * Creates an interactor. This constructor must initialise the interaction. The interactor is (de-)activated if the given
@@ -35,24 +33,24 @@ public abstract class JfxMenuItemInteractor<A extends ActionImpl, I extends Menu
 	 * The class must be public and must have a constructor with no parameter.
 	 * @param clazzInteraction The type of the interaction that will be created. Used to instantiate the interaction by reflexivity.
 	 * The class must be public and must have a constructor with no parameter.
-	 * @param widget The menu item used by the interactor. Cannot be null.
+	 * @param widgets The menu item used by the interactor. Cannot be null.
 	 * @throws IllegalAccessException If no free-parameter constructor is available.
 	 * @throws InstantiationException If an error occurs during instantiation of the interaction/action.
 	 * @throws IllegalArgumentException If the given interaction or instrument is null.
 	 */
-	public JfxMenuItemInteractor(N ins, boolean exec, Class<A> clazzAction, Class<I> clazzInteraction, MenuItem widget) throws InstantiationException, IllegalAccessException {
+	public JfxMenuItemInteractor(N ins, boolean exec, Class<A> clazzAction, Class<I> clazzInteraction, MenuItem... widgets) throws InstantiationException, IllegalAccessException {
 		super(ins, exec, clazzAction, clazzInteraction);
 
-		if(widget==null)
-			throw new IllegalArgumentException();
+		if(widgets == null) throw new IllegalArgumentException();
 
-		menuItem = widget;
-		interaction.registerToMenuItems(Collections.singletonList(widget));
+		menuItem = Arrays.asList(widgets);
+		interaction.registerToMenuItems(menuItem);
 	}
 
 
 	@Override
 	public boolean isConditionRespected() {
-		return interaction.getWidget()==menuItem;
+		final MenuItem widget = interaction.getWidget();
+		return menuItem.stream().anyMatch(menu -> menu == widget);
 	}
 }
