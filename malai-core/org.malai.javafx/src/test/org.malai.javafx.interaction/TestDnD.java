c753d11b9d12e08db7ddb6cf1141c6ba7279e099
arnobl <blouin.arno@gmail.com>
2017-05-01 17:11:09 +0200

    test(interaction): tests added

---
+++ malai-core/org.malai.javafx/src/test/org.malai.javafx.interaction/TestDnD.java
@@ -0,0 +1,46 @@
+package org.malai.javafx.interaction;
+
+import javafx.scene.input.MouseButton;
+import org.junit.Test;
+import org.malai.interaction.InitState;
+import org.malai.javafx.interaction.library.DnD;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestDnD extends TestJfXInteraction<DnD> {
+	@Override
+	protected DnD createInteraction() {
+		return new DnD();
+	}
+
+	@Test
+	public void testNoiseBeforeDnD() {
+		interaction.onPressure(createMousePressEvent(10d, 20d, MouseButton.PRIMARY), 0);
+		interaction.onRelease(createMouseReleaseEvent(10d, 20d, MouseButton.PRIMARY), 0);
+		interaction.onMove(createMouseMoveEvent(11d, 21d, MouseButton.PRIMARY), 0);
+		interaction.onMove(createMouseMoveEvent(10d, 22d, MouseButton.PRIMARY), 0);
+		interaction.onMove(createMouseMoveEvent(10d, 23d, MouseButton.PRIMARY), 0);
+		interaction.onPressure(createMousePressEvent(30d, 40d, MouseButton.SECONDARY), 0);
+		interaction.onRelease(createMouseReleaseEvent(30d, 40d, MouseButton.SECONDARY), 0);
+
+		assertTrue(interaction.getCurrentState() instanceof InitState);
+	}
+
+	@Test
+	public void testCorrectButtonNoiseBeforeDnD() {
+		interaction.onPressure(createMousePressEvent(10d, 20d, MouseButton.PRIMARY), 0);
+		interaction.onRelease(createMouseReleaseEvent(10d, 20d, MouseButton.PRIMARY), 0);
+		interaction.onMove(createMouseMoveEvent(11d, 21d, MouseButton.PRIMARY), 0);
+		interaction.onMove(createMouseMoveEvent(10d, 22d, MouseButton.PRIMARY), 0);
+//		interaction.onMove(createMouseMoveEvent(10d, 23d, MouseButton.PRIMARY), 0);
+		interaction.onPressure(createMousePressEvent(30d, 40d, MouseButton.SECONDARY), 0);
+		interaction.onRelease(createMouseReleaseEvent(30d, 40d, MouseButton.SECONDARY), 0);
+		interaction.onMove(createMouseMoveEvent(11d, 21d, MouseButton.PRIMARY), 0);
+		interaction.onMove(createMouseMoveEvent(10d, 22d, MouseButton.PRIMARY), 0);
+
+		interaction.onPressure(createMousePressEvent(10d, 20d, MouseButton.PRIMARY), 0);
+
+		assertEquals(MouseButton.PRIMARY, interaction.getButton().orElse(MouseButton.NONE));
+	}
+}
