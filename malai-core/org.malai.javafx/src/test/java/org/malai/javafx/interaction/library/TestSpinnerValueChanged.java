fa7dc2c2673d89d6f0a3cbb6b157a8518a61e075
arnobl <1052622+arnobl@users.noreply.github.com>
2017-11-28 16:21:55 +0100

    testWindowClosed_SpinnerValueChanged (#7)

--- malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/TestSpinnerValueChanged.java
+++ malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/TestSpinnerValueChanged.java
@@ -2,8 +2,11 @@ package org.malai.javafx.interaction.library;
 
 import javafx.scene.control.Spinner;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.malai.javafx.MockitoExtension;
+import org.malai.stateMachine.MustAbortStateMachineException;
+import org.mockito.Mockito;
 
 @ExtendWith(MockitoExtension.class)
 public class TestSpinnerValueChanged extends BaseJfXInteractionTest<SpinnerValueChanged> {
@@ -19,5 +22,35 @@ public class TestSpinnerValueChanged extends BaseJfXInteractionTest<SpinnerValue
 	@Override
 	protected SpinnerValueChanged createInteraction() {
 		return new SpinnerValueChanged();
+	}
+
+	@Test
+	public void testSpinnerChangedGoodState() throws MustAbortStateMachineException {
+		interaction.onJfxSpinnerValueChanged(spinner);
+		sleep(500);
+		Mockito.verify(handler, Mockito.times(1)).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.times(1)).interactionStarts(interaction);
+		Mockito.verify(handler, Mockito.times(1)).interactionUpdates(interaction);
+	}
+
+	@Test
+	public void testSpinnerChange2TimesGoodState() throws MustAbortStateMachineException {
+		interaction.onJfxSpinnerValueChanged(spinner);
+		interaction.onJfxSpinnerValueChanged(spinner);
+		sleep(500);
+		Mockito.verify(handler, Mockito.times(1)).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.times(1)).interactionStarts(interaction);
+		Mockito.verify(handler, Mockito.times(2)).interactionUpdates(interaction);
+	}
+
+	@Test
+	public void testSpinnerChangeTwoTimesWith500GoodState() throws MustAbortStateMachineException {
+		interaction.onJfxSpinnerValueChanged(spinner);
+		sleep(500);
+		interaction.onJfxSpinnerValueChanged(spinner);
+		sleep(500);
+		Mockito.verify(handler, Mockito.times(2)).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.times(2)).interactionStarts(interaction);
+		Mockito.verify(handler, Mockito.times(2)).interactionUpdates(interaction);
 	}
 }
