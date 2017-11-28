ee234cef935238c586b983dcc20349c7910f96da
arnobl <1052622+arnobl@users.noreply.github.com>
2017-11-28 15:06:06 +0100

    Unit Tests of the ToggleButton Class (#4)

--- malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/TestToggleButtonPressed.java
+++ malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/TestToggleButtonPressed.java
@@ -1,8 +1,23 @@
 package org.malai.javafx.interaction.library;
 
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ToggleButton;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.malai.interaction.InitState;
+import org.malai.interaction.Interaction;
+import org.malai.javafx.MockitoExtension;
+import org.malai.stateMachine.MustAbortStateMachineException;
+import org.mockito.Mockito;
 
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
 public class TestToggleButtonPressed extends BaseJfXInteractionTest<ToggleButtonPressed> {
 	ToggleButton button;
 
@@ -16,5 +31,72 @@ public class TestToggleButtonPressed extends BaseJfXInteractionTest<ToggleButton
 	@Override
 	protected ToggleButtonPressed createInteraction() {
 		return new ToggleButtonPressed();
+	}
+
+	@Test
+	void testToggleButtonClickGoodState() throws MustAbortStateMachineException {
+		interaction.onJfxToggleButtonPressed(button);
+		Mockito.verify(handler, Mockito.times(1)).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.times(1)).interactionStarts(interaction);
+	}
+
+	@Test
+	void testToggleButtonClickGoodData() throws MustAbortStateMachineException {
+		interaction.addHandler(new InteractionHandlerStub() {
+			@Override
+			public void interactionStops(final Interaction interaction) throws MustAbortStateMachineException {
+				super.interactionStops(interaction);
+				assertEquals(button, ((ToggleButtonPressed) interaction).widget);
+			}
+		});
+
+		interaction.onJfxToggleButtonPressed(button);
+	}
+
+	@Test
+	void testToggleButtonClickReinit() throws MustAbortStateMachineException {
+		interaction.onJfxToggleButtonPressed(button);
+		assertNull(interaction.getWidget());
+		assertEquals(-1, interaction.getLastHIDUsed());
+		assertTrue(interaction.getCurrentState() instanceof InitState);
+	}
+
+	@Test
+	void testRegisterToggleButtons() throws MustAbortStateMachineException {
+		interaction.registerToNodes(Collections.singletonList(button));
+		button.fire();
+		Mockito.verify(handler, Mockito.times(1)).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.times(1)).interactionStarts(interaction);
+	}
+
+	@Test
+	void testNoActionWhenNotRegisteredToggleButton() throws MustAbortStateMachineException {
+		button.fire();
+		Mockito.verify(handler, Mockito.never()).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionStarts(interaction);
+	}
+
+	@Test
+	void testNoActionWhenNotToggleButtonRegistered() throws MustAbortStateMachineException {
+		interaction.registerToNodes(Collections.singletonList(new CheckBox()));
+		button.fire();
+		Mockito.verify(handler, Mockito.never()).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionStarts(interaction);
+	}
+
+	@Test
+	void testToggleButtonNoActionWhenNullRegistered() throws MustAbortStateMachineException {
+		interaction.registerToNodes(null);
+		button.fire();
+		Mockito.verify(handler, Mockito.never()).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionStarts(interaction);
+	}
+
+	@Test
+	void testToggleButtonNoActionWhenContainsNullRegistered() throws MustAbortStateMachineException {
+		interaction.registerToNodes(Collections.singletonList(null));
+		button.fire();
+		Mockito.verify(handler, Mockito.never()).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionStarts(interaction);
 	}
 }
