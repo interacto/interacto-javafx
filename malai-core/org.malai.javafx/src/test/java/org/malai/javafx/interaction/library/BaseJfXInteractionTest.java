831a2abe977b8c375a95c8352b19066d9670b545
arnobl <1052622+arnobl@users.noreply.github.com>
2017-11-29 16:21:49 +0100

    test(interaction): tests added

--- malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/BaseJfXInteractionTest.java
+++ malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/BaseJfXInteractionTest.java
@@ -1,14 +1,18 @@
 package org.malai.javafx.interaction.library;
 
+import javafx.application.Platform;
 import javafx.event.EventType;
 import javafx.geometry.Point2D;
 import javafx.geometry.Point3D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.PickResult;
 import javafx.scene.input.ScrollEvent;
+import javafx.stage.Stage;
 import javafx.stage.Window;
 import javafx.stage.WindowEvent;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +21,9 @@ import org.malai.interaction.InteractionHandler;
 import org.malai.javafx.interaction.JfxInteraction;
 import org.malai.stateMachine.MustAbortStateMachineException;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
 
 public abstract class BaseJfXInteractionTest<T extends JfxInteraction> extends ApplicationTest {
 	T interaction;
@@ -32,43 +38,60 @@ public abstract class BaseJfXInteractionTest<T extends JfxInteraction> extends A
 
 	abstract T createInteraction();
 
-	WindowEvent createWindowEvent(final Window window, final EventType<?> eventType) {
+	Window createWindow() {
+		Scene scene = new Scene(new Button());
+		Platform.runLater(() -> {
+			Stage stage = new Stage();
+			stage.setScene(scene);
+		});
+		WaitForAsyncUtils.waitForFxEvents();
+		return scene.getWindow();
+	}
+
+	void checkNoUseOfHandler() throws MustAbortStateMachineException {
+		Mockito.verify(handler, Mockito.never()).interactionStarts(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionUpdates(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionAborts(interaction);
+	}
+
+	static WindowEvent createWindowEvent(final Window window, final EventType<?> eventType) {
 		return new WindowEvent(window, eventType);
 	}
 
-	MouseEvent createMousePressEvent(final double x, final double y, final MouseButton button) {
+	static MouseEvent createMousePressEvent(final double x, final double y, final MouseButton button) {
 		return new MouseEvent(MouseEvent.MOUSE_PRESSED, x, y, 0d, 0d, button, 1, false,
 			false, false, false, false, false, false,
 			true,false, false, new PickResult(null, new Point3D(x, y, 0d),
 			0d, 0, new Point2D(0d, 0d)));
 	}
 
-	MouseEvent createMouseReleaseEvent(final double x, final double y, final MouseButton button) {
+	static MouseEvent createMouseReleaseEvent(final double x, final double y, final MouseButton button) {
 		return new MouseEvent(MouseEvent.MOUSE_RELEASED, x, y, 0d, 0d, button, 1, false,
 			false, false, false, false, false, false,
 			true,false, false, new PickResult(null, new Point3D(x, y, 0d),
 			0d, 0, new Point2D(0d, 0d)));
 	}
 
-	MouseEvent createMouseMoveEvent(final double x, final double y, final MouseButton button) {
+	static MouseEvent createMouseMoveEvent(final double x, final double y, final MouseButton button) {
 		return new MouseEvent(MouseEvent.MOUSE_MOVED, x, y, 0d, 0d, button, 1, false,
 			false, false, false, false, false, false,
 			true,false, false, new PickResult(null, new Point3D(x, y, 0d),
 			0d, 0, new Point2D(0d, 0d)));
 	}
 
-	ScrollEvent createScrollEvent(final double x, final double y, final double dx, final double dy) {
+	static ScrollEvent createScrollEvent(final double x, final double y, final double dx, final double dy) {
 		return new ScrollEvent(ScrollEvent.SCROLL, x, y, x, y, false, false, false, false, false, false, dx, dy, dx, dy,
 				null, 0d, null, 0d, 0, new PickResult(null, new Point3D(x, y, 0d),
 				0d, 0, new Point2D(0d, 0d)));
 	}
 
-	KeyEvent createKeyPressEvent(final String str, final KeyCode code) {
+	static KeyEvent createKeyPressEvent(final String str, final KeyCode code) {
 		return new KeyEvent(new Object(), tail -> null, KeyEvent.KEY_PRESSED, str, str, code, false,
 			false, false, false);
 	}
 
-	KeyEvent createKeyReleaseEvent(final String str, final KeyCode code) {
+	static KeyEvent createKeyReleaseEvent(final String str, final KeyCode code) {
 		return new KeyEvent(new Object(), tail -> null, KeyEvent.KEY_RELEASED, str, str, code, false,
 			false, false, false);
 	}
--- malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/TestPress.java
+++ malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction/library/TestPress.java
@@ -1,8 +1,129 @@
 package org.malai.javafx.interaction.library;
 
+import java.util.Collections;
+import javafx.geometry.Point3D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Window;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.malai.interaction.Interaction;
+import org.malai.javafx.MockitoExtension;
+import org.malai.stateMachine.MustAbortStateMachineException;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class TestPress extends BaseJfXInteractionTest<Press> {
 	@Override
 	protected Press createInteraction() {
 		return new Press();
+	}
+
+	@Test
+	void TestPressOK() throws MustAbortStateMachineException {
+		interaction.onPressure(createMousePressEvent(10d, 20d, MouseButton.PRIMARY), 0);
+		Mockito.verify(handler, Mockito.times(1)).interactionStarts(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionUpdates(interaction);
+		Mockito.verify(handler, Mockito.times(1)).interactionStops(interaction);
+		Mockito.verify(handler, Mockito.never()).interactionAborts(interaction);
+	}
+
+	@Test
+	void TestPressGoodData() throws MustAbortStateMachineException {
+		interaction.addHandler(new InteractionHandlerStub() {
+			@Override
+			public void interactionStarts(final Interaction interaction) throws MustAbortStateMachineException {
+				super.interactionStarts(interaction);
+				assertEquals(new Point3D(10d, 20d, 0d), ((Press) interaction).getSrcPoint().get());
+				assertEquals(MouseButton.SECONDARY, ((Press) interaction).getButton().get());
+			}
+		});
+		interaction.onPressure(createMousePressEvent(10d, 20d, MouseButton.SECONDARY), 0);
+	}
+
+	@Nested
+	class onWidget {
+		Canvas canvas;
+
+		@BeforeEach
+		void setUp() {
+			canvas = new Canvas();
+		}
+
+		@Test
+		void TestPressOKRegistration() throws MustAbortStateMachineException {
+			interaction.registerToNodes(Collections.singletonList(canvas));
+			canvas.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+
+			Mockito.verify(handler, Mockito.times(1)).interactionStarts(interaction);
+			Mockito.verify(handler, Mockito.never()).interactionUpdates(interaction);
+			Mockito.verify(handler, Mockito.times(1)).interactionStops(interaction);
+			Mockito.verify(handler, Mockito.never()).interactionAborts(interaction);
+		}
+
+		@Test
+		void TestPressKONoRegistration() throws MustAbortStateMachineException {
+			canvas.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+			checkNoUseOfHandler();
+		}
+
+		@Test
+		void TestPressKONULLRegistration() throws MustAbortStateMachineException {
+			interaction.registerToNodes(null);
+			canvas.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+			checkNoUseOfHandler();
+		}
+
+		@Test
+		void TestPressKOListContainsNULLRegistration() throws MustAbortStateMachineException {
+			interaction.registerToNodes(Collections.singletonList(null));
+			canvas.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+			checkNoUseOfHandler();
+		}
+	}
+
+	@Nested
+	class onWinwdow {
+		Window window;
+
+		@BeforeEach
+		void setUp() {
+			window = createWindow();
+		}
+
+		@Test
+		void TestPressOKRegistration() throws MustAbortStateMachineException {
+			interaction.registerToWindows(Collections.singletonList(window));
+			window.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+
+			Mockito.verify(handler, Mockito.times(1)).interactionStarts(interaction);
+			Mockito.verify(handler, Mockito.never()).interactionUpdates(interaction);
+			Mockito.verify(handler, Mockito.times(1)).interactionStops(interaction);
+			Mockito.verify(handler, Mockito.never()).interactionAborts(interaction);
+		}
+
+		@Test
+		void TestPressKONoRegistration() throws MustAbortStateMachineException {
+			window.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+			checkNoUseOfHandler();
+		}
+
+		@Test
+		void TestPressKONULLRegistration() throws MustAbortStateMachineException {
+			interaction.registerToWindows(null);
+			window.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+			checkNoUseOfHandler();
+		}
+
+		@Test
+		void TestPressKOListContainsNULLRegistration() throws MustAbortStateMachineException {
+			interaction.registerToWindows(Collections.singletonList(null));
+			window.fireEvent(createMousePressEvent(11d, 23d, MouseButton.MIDDLE));
+			checkNoUseOfHandler();
+		}
 	}
 }
