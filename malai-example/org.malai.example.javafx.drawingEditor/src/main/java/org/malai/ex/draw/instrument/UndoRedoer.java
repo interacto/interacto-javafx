ea3cb68fa86f66ce9dabec6cc6bf194637116674
arnobl <blouin.arno@gmail.com>
2017-12-04 18:13:49 +0100

    update(example): code simplification

--- malai-example/org.malai.example.javafx.drawingEditor/src/main/java/org/malai/ex/draw/instrument/UndoRedoer.java
+++ malai-example/org.malai.example.javafx.drawingEditor/src/main/java/org/malai/ex/draw/instrument/UndoRedoer.java
@@ -39,7 +39,7 @@ public class UndoRedoer extends JfxInstrument implements Initializable {
 	@Override
 	protected void configureBindings() throws InstantiationException, IllegalAccessException {
 		// Undo and Redo are actions provided by Malai.
-		buttonBinder(Undo.class).on(undoB).when(i -> UndoCollector.INSTANCE.getLastUndo().isPresent()).bind();
-		buttonBinder(Redo.class).on(redoB).when(i -> UndoCollector.INSTANCE.getLastRedo().isPresent()).bind();
+		buttonBinder(Undo.class).on(undoB).bind();
+		buttonBinder(Redo.class).on(redoB).bind();
 	}
 }
