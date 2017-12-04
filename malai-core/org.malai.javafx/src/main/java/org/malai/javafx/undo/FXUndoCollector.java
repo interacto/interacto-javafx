124d59ba6771cfc060f27cd13e50f3f6a7762b81
arnobl <blouin.arno@gmail.com>
2017-12-04 15:11:00 +0100

    new(undo): using mala-jfx last undo and redo actions are properties

---
+++ malai-core/org.malai.javafx/src/main/java/org/malai/javafx/undo/FXUndoCollector.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of Malai.
+ * Copyright (c) 2005-2017 Arnaud BLOUIN
+ * Malai is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * Malai is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+package org.malai.javafx.undo;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import org.malai.undo.UndoCollector;
+import org.malai.undo.UndoHandler;
+import org.malai.undo.Undoable;
+
+/**
+ * Additional routines to handle undoable redoable actions in JavaFX.
+ */
+public final class FXUndoCollector {
+	/** The singleton. */
+	public static final FXUndoCollector INSTANCE = new FXUndoCollector();
+
+	private final ObjectProperty<Undoable> lastUndo;
+	private final ObjectProperty<Undoable> lastRedo;
+
+	private FXUndoCollector() {
+		super();
+		lastUndo = new SimpleObjectProperty<>();
+		lastRedo = new SimpleObjectProperty<>();
+		UndoCollector.INSTANCE.addHandler(new FXUndoHander());
+	}
+
+	public ReadOnlyObjectProperty<Undoable> lastUndoProperty() {
+		return lastUndo;
+	}
+
+	public ReadOnlyObjectProperty<Undoable> lastRedoProperty() {
+		return lastRedo;
+	}
+
+	private void updateLastUndoRedo() {
+		lastUndo.set(UndoCollector.INSTANCE.getLastUndo());
+		lastRedo.set(UndoCollector.INSTANCE.getLastRedo());
+	}
+
+	class FXUndoHander implements UndoHandler {
+		@Override
+		public void onUndoableCleared() {
+			updateLastUndoRedo();
+		}
+
+		@Override
+		public void onUndoableAdded(final Undoable undoable) {
+			updateLastUndoRedo();
+		}
+
+		@Override
+		public void onUndoableUndo(final Undoable undoable) {
+			updateLastUndoRedo();
+		}
+
+		@Override
+		public void onUndoableRedo(final Undoable undoable) {
+			updateLastUndoRedo();
+		}
+	}
+}
