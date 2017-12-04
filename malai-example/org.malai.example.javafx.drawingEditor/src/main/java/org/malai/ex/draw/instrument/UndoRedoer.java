6326e7c96ba7648e5a13ecf77d3a927dfe222ccc
arnobl <blouin.arno@gmail.com>
2017-12-04 15:11:30 +0100

    update(example): update to use the latest API

--- malai-example/org.malai.example.javafx.drawingEditor/src/main/java/org/malai/ex/draw/instrument/UndoRedoer.java
+++ malai-example/org.malai.example.javafx.drawingEditor/src/main/java/org/malai/ex/draw/instrument/UndoRedoer.java
@@ -5,12 +5,11 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Tooltip;
 import org.malai.action.library.Redo;
 import org.malai.action.library.Undo;
 import org.malai.javafx.instrument.JfxInstrument;
+import org.malai.javafx.undo.FXUndoCollector;
 import org.malai.undo.UndoCollector;
-import org.malai.undo.Undoable;
 
 /**
  * Manages undos and redos.
@@ -25,7 +24,9 @@ public class UndoRedoer extends JfxInstrument implements Initializable {
 	public void initialize(final URL location, final ResourceBundle resources) {
 		setActivated(true);
 		UndoCollector.INSTANCE.addHandler(this);
-		updateWidgets();
+
+		undoB.disableProperty().bind(FXUndoCollector.INSTANCE.lastUndoProperty().isNull());
+		redoB.disableProperty().bind(FXUndoCollector.INSTANCE.lastRedoProperty().isNull());
 	}
 
 	@Override
@@ -33,40 +34,5 @@ public class UndoRedoer extends JfxInstrument implements Initializable {
 		// Undo and Redo are actions provided by Malai.
 		buttonBinder(Undo.class).on(undoB).when(i -> UndoCollector.INSTANCE.getLastUndo() != null).bind();
 		buttonBinder(Redo.class).on(redoB).when(i -> UndoCollector.INSTANCE.getLastRedo() != null).bind();
-	}
-
-	public void updateWidgets() {
-		if(isActivated()) {
-			final Undoable undo = UndoCollector.INSTANCE.getLastUndo();
-			final Undoable redo = UndoCollector.INSTANCE.getLastRedo();
-
-			undoB.setDisable(undo == null);
-			redoB.setDisable(redo == null);
-			undoB.setTooltip(undo == null ? null : new Tooltip(undo.getUndoName()));
-			redoB.setTooltip(redo == null ? null : new Tooltip(redo.getUndoName()));
-		}
-	}
-
-	@Override
-	public void onUndoableCleared() {
-		updateWidgets();
-	}
-
-
-	@Override
-	public void onUndoableAdded(final Undoable undoable) {
-		updateWidgets();
-	}
-
-
-	@Override
-	public void onUndoableUndo(final Undoable undoable) {
-		updateWidgets();
-	}
-
-
-	@Override
-	public void onUndoableRedo(final Undoable undoable) {
-		updateWidgets();
 	}
 }
