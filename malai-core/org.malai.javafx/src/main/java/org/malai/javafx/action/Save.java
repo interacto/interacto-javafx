ce2e88c58cd9f2155035a55cdfe8c26f822ebc57
arnobl <1052622+arnobl@users.noreply.github.com>
2017-12-04 18:13:25 +0100

    change(action): change in the action library to use the latest API

--- malai-core/org.malai.javafx/src/main/java/org/malai/javafx/action/Save.java
+++ malai-core/org.malai.javafx/src/main/java/org/malai/javafx/action/Save.java
@@ -10,14 +10,22 @@
  */
 package org.malai.javafx.action;
 
+import java.io.File;
 import java.util.concurrent.ExecutionException;
+import javafx.scene.control.ProgressBar;
 import org.malai.error.ErrorCatcher;
+import org.malai.javafx.ui.JfxUI;
+import org.malai.javafx.ui.OpenSaver;
 
 /**
  * A save action.
  * @author Arnaud Blouin
  */
 public class Save<B> extends IOAction<B> {
+	public Save(final File file, final OpenSaver<B> openSaveManager, final ProgressBar progressBar, final B statusWidget, final JfxUI ui) {
+		super(file, openSaveManager, progressBar, statusWidget, ui);
+	}
+
 	/**
 	 * Creates a save action.
 	 */
