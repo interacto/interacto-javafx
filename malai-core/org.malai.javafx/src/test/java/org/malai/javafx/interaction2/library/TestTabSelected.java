995eecb442a7b416fa17ded107c3e618d1b57f0f
arnobl <1052622+arnobl@users.noreply.github.com>
2018-02-02 17:35:51 +0100

    fix(interaction): tab should have the same behaviour than before

--- malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction2/library/TestTabSelected.java
+++ malai-core/org.malai.javafx/src/test/java/org/malai/javafx/interaction2/library/TestTabSelected.java
@@ -1,9 +1,28 @@
 package org.malai.javafx.interaction2.library;
 
-import javafx.event.ActionEvent;
+import java.util.Collections;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.malai.fsm.CancelFSMException;
+import org.mockito.Mockito;
 
 public class TestTabSelected extends BaseWIMPWidgetTest<TabPane, TabSelected> {
+	Tab tab1;
+	Tab tab2;
+
+	@Override
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+		tab1 = new Tab();
+		tab2 = new Tab();
+		wimpWidget.getTabs().addAll(tab1);
+		wimpWidget.getTabs().addAll(tab2);
+		wimpWidget.getSelectionModel().select(0);
+	}
+
 	@Override
 	TabPane createWidget() {
 		return new TabPane();
@@ -11,11 +30,28 @@ public class TestTabSelected extends BaseWIMPWidgetTest<TabPane, TabSelected> {
 
 	@Override
 	void triggerWidget() {
-		wimpWidget.fireEvent(new ActionEvent(wimpWidget, null));
+		wimpWidget.fireEvent(new TabEvent(wimpWidget, null));
 	}
 
 	@Override
 	protected TabSelected createInteraction() {
 		return new TabSelected();
+	}
+
+	@Override
+	@Test
+	void testProcessEventGoodState() throws CancelFSMException {
+		interaction.processEvent(new TabEvent(wimpWidget, null));
+		Mockito.verify(handler, Mockito.times(1)).fsmStops();
+		Mockito.verify(handler, Mockito.times(1)).fsmStarts();
+	}
+
+	@Override
+	@Test
+	void testRegister() throws CancelFSMException {
+		interaction.registerToNodes(Collections.singletonList(wimpWidget));
+		wimpWidget.getSelectionModel().select(1);
+		Mockito.verify(handler, Mockito.times(1)).fsmStops();
+		Mockito.verify(handler, Mockito.times(1)).fsmStarts();
 	}
 }
