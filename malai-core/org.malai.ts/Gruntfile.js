6d8364ac31961a4c0bc170f6a9af924fa6e28022
arnobl <1052622+arnobl@users.noreply.github.com>
2018-03-20 13:26:01 +0100

    config(ts): ts configuration updated

--- malai-core/org.malai.ts/Gruntfile.js
+++ malai-core/org.malai.ts/Gruntfile.js
@@ -1,28 +1,20 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        ts: {
+            default : {
+                tsconfig: './tsconfig.json',
+                src : ['src-core/**/*.ts', 'src/**/*.ts'],
+                out: 'src-gen/js/malai.js',
+                passThrough: true
+            }
+        },
+        clean: {
+            folder: ['src-gen/js']
+        }
+    });
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-	typescript_project: {
-		options: {
-            tsconfig: "./tsconfig.json"
-		},
-		js_gen: {
-		  // ALWAYS use files, instead of src/dest 
-		  // Note that for EACH dest, will be created a separated TSC project json file, keep that in mind 
-		  files: {
-		    // its a folder == outDir 
-		    'src-gen/js': ['src-core/**/*.ts', 'src/**/*.ts'],
-		    // its a file == outFile 
-		//    'dest/has/extension.js': ['srcs/**/*.ts']
-		  }
-		}
-	  },
-      clean: {
-          folder: ['src-gen/js']
-      }
-  });
-
-  grunt.loadNpmTasks('grunt-typescript-project');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.registerTask("default", ["ts"]);
+    grunt.loadNpmTasks('grunt-contrib-clean');
 };
