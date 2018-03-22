52f9175953344815474d27dfea8370a60aaeb38c
arnobl <1052622+arnobl@users.noreply.github.com>
2018-03-22 21:05:11 +0100

    config(ts): replacing grunt by scripts

--- malai-core/org.malai.ts/test/fsm/Transition.test.ts
+++ malai-core/org.malai.ts/test/fsm/Transition.test.ts
@@ -1,3 +1,14 @@
+/*
+ * This file is part of Malai.
+ * Copyright (c) 2009-2018 Arnaud BLOUIN
+ * Malai is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * Malai is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
+
 import {StdState} from "../../src-core/fsm/StdState";
 import {Transition} from "../../src-core/fsm/Transition";
 import {FSM} from "../../src-core/fsm/FSM";
@@ -11,7 +22,7 @@ let state2: StdState<StubEvent>;
 
 
 beforeEach(() => {
-    let fsm: FSM<StubEvent> = new FSM<StubEvent>();
+    const fsm: FSM<StubEvent> = new FSM<StubEvent>();
     state1 = new StdState<StubEvent>(fsm, "s1");
     state2 = new StdState<StubEvent>(fsm, "s2");
     tr = new StubTransitionOK(state1, state2);
