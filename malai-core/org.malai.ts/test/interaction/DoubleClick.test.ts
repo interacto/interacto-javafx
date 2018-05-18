4ed24b015b98de90b4d668445e7fe67aed4af97e
arnobl <1052622+arnobl@users.noreply.github.com>
2018-05-18 16:51:01 +0200

    Click done, integration of KeyPress (test still needed)

--- malai-core/org.malai.ts/test/interaction/DoubleClick.test.ts
+++ malai-core/org.malai.ts/test/interaction/DoubleClick.test.ts
@@ -38,26 +38,32 @@ beforeEach(() => {
 
 test("Double click on a canvas starts and stops the interaction", () => {
     interaction.registerToNodes([canvas]);
-    canvas.click();
-    canvas.click();
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, canvas));
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseUp, canvas));
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, canvas));
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseUp, canvas));
     expect(handler.fsmStarts).toHaveBeenCalledTimes(1);
     expect(handler.fsmStops).toHaveBeenCalledTimes(1);
 });
 
 test("Move between clicks cancels the double click", () => {
     interaction.registerToNodes([canvas]);
-    canvas.click();
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, canvas));
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseUp, canvas));
     canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseMove, canvas));
-    canvas.click();
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, canvas));
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseUp, canvas));
     expect(handler.fsmStarts).not.toHaveBeenCalled();
     expect(handler.fsmStops).not.toHaveBeenCalled();
 });
 
 test("Timout cancels the double click", () => {
     interaction.registerToNodes([canvas]);
-    canvas.click();
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, canvas));
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseUp, canvas));
     jest.runOnlyPendingTimers();
-    canvas.click();
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseDown, canvas));
+    canvas.dispatchEvent(createMouseEvent(EventRegistrationToken.MouseUp, canvas));
     expect(handler.fsmStarts).not.toHaveBeenCalled();
     expect(handler.fsmStops).not.toHaveBeenCalled();
 });
