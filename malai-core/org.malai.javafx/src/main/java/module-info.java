bc96c60d4a4285cf848530d48e78466cbbede864
arnobl <1052622+arnobl@users.noreply.github.com>
2018-10-11 11:33:02 +0200

    change(java): moving to java 11 and modules

--- malai-core/org.malai.javafx/src/main/java/module-info.java
+++ malai-core/org.malai.javafx/src/main/java/module-info.java
@@ -3,6 +3,14 @@ module malai.javafx {
 	requires javafx.controls;
 	requires java.logging;
 	requires java.xml;
-
 	requires malai.core;
+
+	exports org.malai.javafx.binding;
+	exports org.malai.javafx.command;
+	exports org.malai.javafx.instrument;
+	exports org.malai.javafx.interaction;
+	exports org.malai.javafx.interaction.help;
+	exports org.malai.javafx.interaction.library;
+	exports org.malai.javafx.ui;
+	exports org.malai.javafx.undo;
 }
