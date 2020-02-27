e0f14a5f0d76222f312b941f7941e580894759b6
arnobl <1052622+arnobl@users.noreply.github.com>
2020-02-27 09:22:48 +0100

    governance(license): headers' year updated

--- src/main/java/io/github/interacto/jfx/binding/AnonCmdBinder.java
+++ src/main/java/io/github/interacto/jfx/binding/AnonCmdBinder.java
@@ -1,6 +1,6 @@
 /*
  * Interacto
- * Copyright (C) 2019 Arnaud Blouin
+ * Copyright (C) 2020 Arnaud Blouin
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
