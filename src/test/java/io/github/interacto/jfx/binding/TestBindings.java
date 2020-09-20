7cc2701549f092a06d8114359a8c89866db7fb07
arnobl <1052622+arnobl@users.noreply.github.com>
2020-09-20 23:04:47 +0200

    test(binding): various tests added

--- src/test/java/io/github/interacto/jfx/binding/TestBindings.java
+++ src/test/java/io/github/interacto/jfx/binding/TestBindings.java
@@ -14,29 +14,67 @@
  */
 package io.github.interacto.jfx.binding;
 
+import io.github.interacto.jfx.TimeoutWaiter;
 import io.github.interacto.jfx.binding.api.LogLevel;
 import io.github.interacto.jfx.command.ActivateInactivateInstruments;
 import io.github.interacto.jfx.interaction.library.DnD;
-import io.github.interacto.jfx.interaction.library.SrcTgtPointsData;
+import io.github.interacto.jfx.interaction.library.DragLock;
+import io.github.interacto.jfx.test.BindingsContext;
+import io.github.interacto.jfx.test.WidgetBindingExtension;
 import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.robot.Motion;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(ApplicationExtension.class)
-public class TestBindings {
+@ExtendWith(WidgetBindingExtension.class)
+public class TestBindings implements TimeoutWaiter {
+	Canvas canvas = new Canvas();
+	JfxWidgetBinding<?, ?, ?> binding;
+
+	@Start
+	public void start(final Stage stage) {
+		canvas = new Canvas();
+		canvas.setWidth(10);
+		canvas.setHeight(10);
+		final VBox parent = new VBox();
+		parent.getChildren().add(canvas);
+		final Scene scene = new Scene(parent);
+		stage.setScene(scene);
+		stage.show();
+		stage.toFront();
+		stage.centerOnScreen();
+		stage.requestFocus();
+	}
+
+	@AfterEach
+	void tearDown() {
+		if(binding != null) {
+			binding.uninstallBinding();
+		}
+	}
+
 	@Test
 	void testBinder() {
-		final JfxWidgetBinding<ActivateInactivateInstruments, DnD, SrcTgtPointsData> binding =
+		binding =
 			Bindings.nodeBinder()
 				.continuousExecution()
 				.on(new Button())
-				.on(new CheckBox())
+				.on(canvas)
 				.on(FXCollections.observableArrayList(new ToggleButton(), new CheckBox()))
 				.log(LogLevel.BINDING)
 				.strictStart()
@@ -56,5 +94,28 @@ public class TestBindings {
 		assertThat(binding.isActivated()).isTrue();
 		assertThat(binding.getInteraction()).isInstanceOf(DnD.class);
 		assertThat(binding.produces()).isNotNull();
+		assertThat(binding.isStrictStart()).isTrue();
+	}
+
+	@Test
+	void testClickMustNotBlockDragLock(final FxRobot robot, final BindingsContext ctx) {
+		binding = Bindings.nodeBinder()
+			.usingInteraction(DragLock::new)
+			.on(canvas)
+			.toProduce(TestBinder.StubCmd::new)
+			.when(i -> i.getButton() == MouseButton.SECONDARY)
+			.log(LogLevel.INTERACTION, LogLevel.BINDING, LogLevel.COMMAND)
+			.strictStart()
+			.bind();
+
+		robot.clickOn(canvas, Motion.DEFAULT, MouseButton.PRIMARY);
+		waitForTimeoutTransitions();
+		robot.clickOn(canvas, Motion.DEFAULT, MouseButton.SECONDARY);
+		robot.clickOn(canvas, Motion.DEFAULT, MouseButton.SECONDARY);
+		robot.moveBy(1, 1);
+		robot.clickOn(canvas, Motion.DEFAULT, MouseButton.SECONDARY);
+		robot.clickOn(canvas, Motion.DEFAULT, MouseButton.SECONDARY);
+
+		ctx.oneCmdProduced(TestBinder.StubCmd.class);
 	}
 }
